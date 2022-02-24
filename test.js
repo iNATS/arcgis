2f490073f0294b9c9d768c5fbd5cdb50f3ff3ada
iNATS <mohamed.inats@gmail.com>
2022-02-24 15:34:45 +0200

    a

---
+++ test.js
@@ -0,0 +1,14 @@
+let cart = [
+  {
+    name: "JavaScript book",
+    price: 4,
+  },
+  {
+    name: "UGG Women's Hazel Ankle Boot",
+    price: 79,
+  },
+  {
+    name: "OXO Good Grips 11-Inch Balloon Whisk",
+    price: 9,
+  },
+];
